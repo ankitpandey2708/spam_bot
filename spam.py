36822e8a4282c53652054d3daac3a6a34eb401ab
Ankit Pandey <ankitpandey2708@gmail.com>
2017-08-24 09:18:08 +0530

    Update spam.py

--- spam.py
+++ spam.py
@@ -4,12 +4,12 @@ from facepy import GraphAPI   #https://pypi.python.org/pypi/facepy/1.0.9
 #https://developers.facebook.com/tools/explorer/
 #Insert user access token here Make sure that you have checked checkbox labeled as ‘publish_action’.
 token = ""
-facebook = fb.graph.api(token)
-graph1 = GraphAPI(token)
+a = fb.graph.api(token)
+b = GraphAPI(token)
     
 id = input("Enter victim's Facebook id: ")  #https://findmyfbid.com/
 query = str(id) + "/posts?fields=id&limit=5" #max limit of posts =100
-r = graph1.get(query)
+r = b.get(query)
 print(r)
 
 posts = [x['id'] for x in r['data']]
@@ -18,8 +18,8 @@ nos = int(input("Enter number of posts to be spammed with comments: "))
 mess = input("Enter the message to be commented: ")
 if nos <= len(posts):
     for i in posts[:nos]:
-        facebook.publish(cat = "comments", id = i, message = mess) #Comments on each post
-        #facebook.publish(cat = "likes", id = i) #Likes each post
+        a.publish(cat = "comments", id = i, message = mess) #Comments on each post
+        #a.publish(cat = "likes", id = i) #Likes each post
         print("facebook.com/" + str(i).split('_')[0] + "/posts/" + str(i).split('_')[1])
 else:
     print("No of posts should be less than "+str(len(posts)))
